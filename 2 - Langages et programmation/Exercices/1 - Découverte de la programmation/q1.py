c639f04b777858a1b2f1fd34f0dba10f68ac5dac
Florant <florian-picard@fustel-yaounde.net>
2026-09-22 09:27:28 +0100

    cours 09-22

---
+++ 2 - Langages et programmation/Exercices/1 - Découverte de la programmation/q1.py
@@ -0,0 +1,16 @@
+from turtle import *
+
+forward(80)
+left(90)
+forward(80)
+left(90)
+forward(80)
+left(90)
+forward(80)
+left(90)
+
+# Commencer une ligne par un # permet de laisser un commentaire
+# forward(70)
+# le code en commentaire n'est pas exécuté
+done() # Ne pas effacer
+# Ne rien écrire ici
---
+++ 2 - Langages et programmation/Exercices/1 - Découverte de la programmation/q2.py
@@ -0,0 +1,11 @@
+from turtle import *
+
+# for i in range(3):
+#     forward(80)
+#     left(120)
+    
+for i in range(4):
+    forward(80)
+    left(90)
+
+done() # Ne pas effacer, ne pas écrire en dessous
